4bde40b903d6f7e7831841aeb3595480b8bc5a3c
Toloy Juan C. <toloyjc@gmail.com>
2024-10-21 14:25:38 -0300

    Add project files.

---
+++ Services/IWineServices.cs
@@ -0,0 +1,11 @@
+﻿
+using Common.Models;
+
+namespace Services
+{
+    public interface IWineServices
+    {
+        void AddWine(CreateWineDTO createWineDTO);
+        Dictionary<string, int> GetAllWinesStock();
+    }
+}
